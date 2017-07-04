725ee5f88bfc79dcfc5e635cc280572ed9f4840c
Zhentian Wan <answer881215@gmail.com>
2017-07-04 23:23:45 +0300

    init push

---
+++ src/app/au-mask/mask.utils.ts
@@ -0,0 +1,10 @@
+/**
+ * Created by zhentianwan on 04/07/2017.
+ */
+export const  TAB = 9,
+  LEFT_ARROW =	37,
+  RIGHT_ARROW = 39,
+  BACKSPACE = 8,
+  DELETE = 46;
+
+export const SPECIAL_CHARACTERS = [" ", "/", "(", ")", "+", "\/", "-"];
